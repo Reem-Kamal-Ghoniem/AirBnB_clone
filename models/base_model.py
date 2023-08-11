80b5c4ba1292cfd8a75ab01562425c5eec135fc7
Fadyy22 <87955141+Fadyy22@users.noreply.github.com>
2023-08-11 03:35:54 +0300

    Feature/file storage (#1)
    * added file storage engine without unittests
    
    * added file storage engine without unittests

--- models/base_model.py
+++ models/base_model.py
@@ -5,32 +5,34 @@ file includes the BaseModel class
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """class for defining all common
     attributes and methods for other classes"""
+
     def __init__(self, *args, **kwargs):
         """initiallization of BaseModel"""
         if kwargs:
-            if "__class__" in kwargs:
-                del kwargs["__class__"]
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    if key == 'created_at':
-                        self.created_at = \
-                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "created_at" or key == "updated_at":
+                    if key == "created_at":
+                        self.created_at = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        )
                     else:
-                        self.updated_at = \
-                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                else:
+                        self.updated_at = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        )
+                elif key != "__class__":
                     setattr(self, key, value)
 
-                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """returns string representation"""
@@ -39,11 +41,12 @@ class BaseModel:
     def save(self):
         """Updates the updated_at attribute"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary"""
         out_dict = self.__dict__.copy()
-        out_dict['__class__'] = type(self).__name__
-        out_dict['created_at'] = self.created_at.isoformat()
-        out_dict['updated_at'] = self.updated_at.isoformat()
+        out_dict["__class__"] = type(self).__name__
+        out_dict["created_at"] = self.created_at.isoformat()
+        out_dict["updated_at"] = self.updated_at.isoformat()
         return out_dict
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module containing unit tests for file storage class"""
+
+import unittest
+
+from models.base_model import BaseModel
+
+
+class FileStorageTest(unittest.TestCase):
+    """tests for file storage class"""
+
+    model = BaseModel()

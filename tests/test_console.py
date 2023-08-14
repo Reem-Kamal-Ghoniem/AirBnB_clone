c8a498938c911bc32d9658db2367d37153545569
Reem-Kamal-Ghoniem <reemkamal111@gmail.com>
2023-08-13 18:08:26 -0700

    more consule tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,3 +9,23 @@ class test_console(unittest.TestCase):
     def test_prompt(self):
         """testing the prompt"""
         self.assertEqual("(hbnb) ", self.console.prompt)
+
+    def test_create(self):
+        """testting create function"""
+        self.console.onecmd("create BaseModel")
+
+    def test_show(self):
+        """testing show function"""
+        self.console.onecmd("show BaseModel 1234-1234-1234")
+
+    def test_destroy(self):
+        """testting destroy function"""
+        self.console.onecmd("destroy BaseModel 1234-1234-1234")
+
+    def test_all(self):
+        """testing all function"""
+        self.console.onecmd("all BaseModel")
+
+    def test_update(self):
+        """testting update function"""
+        self.console.onecmd("update BaseModel 1234-1234-1234 name 'New Name'")
